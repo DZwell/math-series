cce5b301bf3c1bbe429ea45510a45e93833173dc
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-03-01 15:17:51 -0800

    Add if__name__ block

--- series.py
+++ series.py
@@ -2,14 +2,14 @@ import pytest
 
 
 def fibonacci(n):
-    """Return nth number in fibonacci series."""
+    """Return the nth number of the fibonacci series."""
     if n == 1 or n == 2:
         return 1
     return fibonacci(n - 1) + fibonacci(n - 2)
 
 
 def lucas(n):
-    """Return nth number of lucas series."""
+    """Return the nth number of the lucas series."""
     if n == 1:
         return 2
     if n == 2:
@@ -18,8 +18,21 @@ def lucas(n):
 
 
 def sum_series(n, a=0, b=1):
-    """Return nth number in fibonaccis series unless optional args given, then lucas."""
+    """Return the nth number of either the fibonacci or the lucas series."""
     if a == 0 and b == 1:
         return fibonacci(n)
     if a == 2 and b == 1:
         return lucas(n)
+
+
+if __name__ == '__main__':
+    print('This module defines functions that implement mathematical series.')
+    print('...\n')
+    print(fibonacci.__name__ + '(n):\n')
+    print('\t' + fibonacci.__doc__ + '\n')
+    print(lucas.__name__ + '(n):\n')
+    print('\t' + lucas.__doc__ + '\n')
+    print(sum_series.__name__ + '(n):\n')
+    print('\t' + sum_series.__doc__ + '\n')
+
+
